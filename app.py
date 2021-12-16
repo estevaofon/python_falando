efb99912dd6d534f1c704ad3aff78c790d4f2171
Danilo Marquiori <52891323+DaniloMarquiori@users.noreply.github.com>
2021-12-15 22:09:05 -0300

    [ADD] adicionado arquivos

---
+++ app.py
@@ -0,0 +1,12 @@
+# Importando modulo do Google para converção texto para fala
+import gtts
+# Responsavel por tocar o som gerado
+from playsound import playsound
+
+# Abrindo arquivo criado com as frases
+with open('textos.txt', 'r') as arquivo:
+    for linha in arquivo:
+        # Criando arquivo MP3 e reproduzindo o som
+        frase = gtts.gTTS(linha, lang='pt-br')
+        frase.save('texto.mp3')
+        playsound('texto.mp3')
